da9d08a8ece1aaed8452bf2741c935c0e362591a
1_am_a_geek <tmy0x3@icloud.com>
2015-08-22 14:46:02 +0900

    add APNKit.h

---
+++ APNKit/APNKit.h
@@ -0,0 +1,17 @@
+//
+//  APNKit.h
+//  APNKit
+//
+//  Created by Norikazu on 2015/08/22.
+//  Copyright © 2015年 Norikazu. All rights reserved.
+//
+
+#ifndef APNKit_h
+#define APNKit_h
+
+#import "APNObject.h"
+#import "APNObjectManger.h"
+#import "APNImageLoader.h"
+#import "APNObjectMapping.h"
+
+#endif /* APNKit_h */
